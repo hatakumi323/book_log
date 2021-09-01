b885716ce77d0ca69d1c0f1164b7d53c66c09d1d
hatakumi323 <takuken135@gmail.com>
2021-09-01 17:08:37 +0900

    book_log text

---
+++ src/book_log.php
@@ -0,0 +1,12 @@
+<?php
+
+echo '読書ログを登録してください' . PHP_EOL;
+echo '書籍名：';
+echo fgets(STDIN);
+
+
+// echo '書籍名：銀河鉄道の夜' . PHP_EOL;
+// echo '著者名：宮沢賢治' . PHP_EOL;
+// echo '読書状況：読了' . PHP_EOL;
+// echo '評価：5' . PHP_EOL;
+// echo '感想：本当の幸せとは何だろうか考えさせる作品だった' . PHP_EOL;
---
+++ src/tmp/echo.php
@@ -0,0 +1,6 @@
+
+<?php
+
+echo 'Hi' . PHP_EOL;
+echo 'I love PHP' . PHP_EOL;
+echo 'PHP is great' . PHP_EOL;
