45bfc1194eecde6fdcd1ad310febe4e7f51a7a09
hatakumi323 <takuken135@gmail.com>
2021-09-11 17:15:56 +0900

    validate

--- src/companies/create.php
+++ src/companies/create.php
@@ -33,14 +33,19 @@ function validate($company)
     $errors['name'] = '会社名は255文字以内で入力してください';
   }
   // 設立日
+  $dates = explode('-', $company['establishment_date']);
   if (!strlen($company['establishment_date'])) {
     $errors['establishment_date'] = '設立日を入力してください';
+  } elseif (count($dates) !== 3) {
+    $errors['establishment_date'] = '設立日を正しい形式で入力してください';
+  } elseif (!checkdate($dates[1], $dates[2], $dates[0])) {
+    $errors['establishment_date'] = '設立日を正しい日付で入力してください';
   }
-  // 創業者
+  // 代表者
   if (!strlen($company['founder'])) {
-    $errors['founder'] = '創業者名を入力してください';
+    $errors['founder'] = '代表者を入力してください';
   } elseif (strlen($company['founder']) > 255) {
-    $errors['founder'] = '創業者名は255文字以内で入力してください';
+    $errors['founder'] = '代表者は255文字以内で入力してください';
   }
 
   return $errors;
